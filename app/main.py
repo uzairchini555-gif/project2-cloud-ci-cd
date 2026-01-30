0ae4f04ef5bbebfcfb3993bd4ee16979a6d34ccb
EC2 Default User <ec2-user@ip-172-31-37-157.eu-north-1.compute.internal>
2026-01-30 14:27:14 +0000

    Fix python syntzx Error

--- app/main.py
+++ app/main.py
@@ -2,11 +2,7 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
 def root():
-	return {
-	    "Project1": "Project2",
-            "message": "CI/CD pipeline: again testing"
- 	    "Status": "Running"
-	       }
+	return {"message": "Project 2 is running with CI/CD"}
 @app.get("/health")
 def health():
 	return {"Status": "Healthy"}
