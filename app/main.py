bf033dcd8a06351d2d15279b4005c63252723a66
EC2 Default User <ec2-user@ip-172-31-37-157.eu-north-1.compute.internal>
2026-01-30 16:14:42 +0000

    Testing CI/CD Auto deploy

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
 def root():
-	return {"Mustafa ka kurta tight ho gaya"}
+	return {"message": "Project 2 CI/CD Auto Deploy is Working!"}
 @app.get("/health")
 def health():
 	return {"Status": "Healthy"}
