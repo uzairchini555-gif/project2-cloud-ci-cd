0d2ece766893d13c24b0df259ac95c415f6a5a22
EC2 Default User <ec2-user@ip-172-31-37-157.eu-north-1.compute.internal>
2026-01-30 16:05:46 +0000

    Testing auto deploy

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
 def root():
-	return {"message": "Project2 testing"}
+	return {"Mustafa ka kurta tight ho gaya"}
 @app.get("/health")
 def health():
 	return {"Status": "Healthy"}
